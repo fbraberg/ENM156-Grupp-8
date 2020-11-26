276b97c56944eefb13859398ffa733cd93cc9aca
Felix342 <fillebraberg@gmail.com>
2020-11-26 16:04:48 +0100

    Added basic stations and json functions

--- app/src/main/java/com/example/reviewer/IntroAboutActivity.java
+++ app/src/main/java/com/example/reviewer/IntroAboutActivity.java
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+
+
+import java.util.logging.Logger;
 
 /* Displays information about the app. */
 public class IntroAboutActivity extends AppCompatActivity {
+    private static final String tag = "Testing: ";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +27,13 @@ public class IntroAboutActivity extends AppCompatActivity {
         // Open map view
         Intent intent = new Intent(IntroAboutActivity.this, MapsActivity.class);
         startActivity(intent);
+        try {
+            io.io.readStation(null, null);
+        } catch (Exception e) {
+            Log.v("ERROR", e.getMessage());
+        }
+
+
+
     }
 }
--- app/src/main/java/io/io.java
+++ app/src/main/java/io/io.java
@@ -1,6 +1,93 @@
 package io;
 
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.io.IOException;
+
+
 
 public class io {
+
+
+
+    public String[] readStations (String path) throws JSONException {
+        return null;
+    }
+
+    public static String readStation (String path, String name) throws JSONException {
+
+        JSONObject reader = new JSONObject("{\n" +
+                "    \"stations\": \n" +
+                "    [\n" +
+                "        {\n" +
+                "            \"name\" : \"Kungsportsplatsen\",\n" +
+                "            \"image\" : \"----------\",\n" +
+                "            \"ratings\" : \n" +
+                "            [\n" +
+                "                {\"stars\": 2},\n" +
+                "                {\"stars\": 5}\n" +
+                "            ],\n" +
+                "            \"reports\" : \n" +
+                "            [\n" +
+                "                {\n" +
+                "                    \"category\" : \"opinion\",\n" +
+                "                    \"comment\" : \"Hållplatsen är sämst\",\n" +
+                "                    \"picture\" : \"-----\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"category\" : \"Suggestion\",\n" +
+                "                    \"comment\" : \"Måla den röd\",\n" +
+                "                    \"picture\" : \"-----\"\n" +
+                "                }\n" +
+                "            ]\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"name\" : \"Nordstan\",\n" +
+                "            \"image\" : \"----------\",\n" +
+                "            \"ratings\" : \n" +
+                "            [\n" +
+                "                {\"stars\": \"-----\"}\n" +
+                "            ],\n" +
+                "            \"reports\" : \n" +
+                "            [\n" +
+                "                {\n" +
+                "                    \"category\" : \"-----\",\n" +
+                "                    \"comment\" : \"-----\",\n" +
+                "                    \"picture\" : \"-----\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"category\" : \"-----\",\n" +
+                "                    \"comment\" : \"-----\",\n" +
+                "                    \"picture\" : \"-----\"\n" +
+                "                }\n" +
+                "            ]\n" +
+                "        }\n" +
+                "    ]\n" +
+                "\n" +
+                "}");
+        JSONArray stations = reader.getJSONArray("stations");
+
+        for (int i = 0; i < stations.length(); i++) {
+            JSONObject c = stations.getJSONObject(i);
+            Log.v("STAT" + i, c.getString("name"));
+        }
+
+
+        return null;
+    }
+
+    public void writeStations (String path) {
+
+    }
+
+    public void writeStation (String path) {
+
+    }
+
 }
